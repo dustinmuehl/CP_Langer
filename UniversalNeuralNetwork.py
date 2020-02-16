fefd6e9b31ec3c8014fe679f9cd65b6a46965d1d
Smakinski <56673589+Smakinski@users.noreply.github.com>
2020-02-16 10:50:51 +0100

    Add files via upload

--- UniversalNeuralNetwork.py
+++ UniversalNeuralNetwork.py
@@ -1,7 +1,52 @@
 import numpy as np
+import gzip # Entzipper für das Auslesen der Testdaten
+import pickle #Einlesen der Testdaten in einen Array
+import os.path
+
+with gzip.open('mnist.pkl.gz', 'rb') as f: #Öffnet das Trainingsdatenset
+        train_set, valid_set, test_set = pickle.load(f, encoding='iso-8859-1')
+        #train_set[0] ist eine 50.0000 x 784 Matrix --> Pixel Daten
+        #train_set[1] ist eine 50.0000 x 1 Matrix --> Wahre Werte
+        #encoding='iso-8859-1' wird benötigt, da ab python 3 ein anderer 
+        #Kodierungsstandard Verwendung findet und das Trainingsdatenset in 
+        #Python 2 geschrieben wurde 
+
+#überführe train_x in np Array
+train_x = np.zeros((len(train_set[0]),784))
+for i in range(len(train_set[0])): #Pixeldatenset  
+    train_x[i,:]=np.asarray(train_set[0][i])
+
+train_y = train_set[1] #Ziffern in Computerdarstellung
+
+#überführe test_x in np Array
+test_x = np.zeros((len(test_set[0]),784))
+for i in range(len(test_set[0])): #Pixeldatenset  
+    test_x[i,:]=np.asarray(test_set[0][i])
+
+test_y = test_set[1]
+
+
+#überführe Ziffern in Vektoren der passenden Form
+train_y_dec = np.zeros([len(train_y),10])
+for i in range(len(train_y)):
+    train_y_dec[i][train_y[i]] = 1
+    
+test_y_dec = np.zeros([len(test_y),10])
+for i in range(len(test_y)):
+    test_y_dec[i][test_y[i]] = 1
+
+#Sigmoid-Funktion
+def sig(x):
+    return 1/(1 + np.exp(-x))
+    
+
+#Ableitung der Sigmoid-Funktion
+def sigder(x):
+    return sig(x)*(1-sig(x))
+
 
 class UniversalNeuralNetwork:
-    def __init__(self,L):
+    def __init__(self,L,c):
         #L ist eine Liste, die die Größen der einzelnen Layers enthält
         self.layercount = len(L)        #Anzahl der Layers
         
@@ -31,7 +76,7 @@ class UniversalNeuralNetwork:
 
         J3 = []         #Liste der Gewichtsmatrizen
         for i in range(self.layercount -1):
-            J3.append(np.random.rand(L[i+1],self.layersize[i]))
+            J3.append(c*np.random.rand(L[i+1],self.layersize[i]))
         self.weights = J3    
         
         
@@ -42,8 +87,8 @@ class UniversalNeuralNetwork:
         
         K1.append(np.concatenate((x,self.bias)))
         for i in range(self.layercount-2):
-            K1.append(np.concatenate((sig(np.dot(self.weights[i], self.V[i])),self.bias)))
-        K1.append(sig(np.dot(self.weights[self.layercount -2], self.V[self.layercount -2])))
+            K1.append(np.concatenate((sig(np.dot(self.weights[i], K1[i])),self.bias)))
+        K1.append(sig(np.dot(self.weights[self.layercount -2], K1[self.layercount -2])))
         self.V = K1
                   
         for i in range(self.layercount -1):
@@ -60,7 +105,7 @@ class UniversalNeuralNetwork:
         
         a=[]
         for item in self.A:
-            a.append(np.diag(item))    
+            a.append(np.diag(sigder(item)))    
         
         h = self.V[self.layercount -1]    
         
@@ -69,9 +114,9 @@ class UniversalNeuralNetwork:
             o.append(self.V[i])
         
         delta = []
-        delta.insert(0,np.dot(sigder(a[self.layercount -2]),(h-y)))
+        delta.insert(0,np.dot(a[self.layercount -2],(h-y)))
         for i in range(self.layercount -2):
-            delta.insert(0, np.dot(sigder(a[self.layercount -3-i]),np.dot(delta[0].T, W[self.layercount-3-i]).T))
+            delta.insert(0, np.dot(a[self.layercount -3-i],np.dot(delta[0].T, W[self.layercount-3-i]).T))
         
         gradw = []
         for i in range(self.layercount -1):
@@ -93,8 +138,9 @@ class UniversalNeuralNetwork:
             i = k*m
             for l in range(m):
                 self.feedforward(x[:,N[i]])
+                backprop=self.backprop(y[:,N[i]])
                 for j in range(self.layercount -1):
-                    gradw[j] = gradw[j] + self.backprop(y[:,N[i]])[j]
+                    gradw[j] = gradw[j] + backprop[j]
                 i = i +1
             
             for j in range(self.layercount-1):
@@ -123,3 +169,25 @@ class UniversalNeuralNetwork:
             if np.argmax(self.V[self.layercount-1])==y[j]:
                 summe=summe+1
         print(summe,"/",x.shape[1],"erkannt")
+
+###################################################################################################################
+g=[784,30,10]
+
+n=UniversalNeuralNetwork(g,0.01)
+
+n.feedforward(test_x.T[:,4001])
+
+print("output = ", n.V[n.layercount-1])
+print("richtig = ", test_y[4001])
+
+for i in range(5):
+    #c=1/(0.01*(25+i))
+    n.train(train_x.T ,train_y_dec.T , 10, 5000, 0.3)
+    #n.traindumm(train_x.T ,train_y_dec.T , 0.3)
+    print("Epoche",i)
+    #n.test1(test_x.T, test_y_dec.T)
+    n.test2(test_x.T, test_y.T)
+
+n.feedforward(test_x.T[:,4001])
+
+print("output = ", n.V[n.layercount-1])
